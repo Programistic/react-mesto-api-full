6a49bf0bd64321a7ae188842792fab790cd353ea
Maksim Dav <maksim.dav@mail.ru>
2022-07-21 22:43:22 +0800

    fix headers

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -4,7 +4,7 @@ export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json; charset=utf-8',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ email, password }),
   })
@@ -15,7 +15,7 @@ export const authorize = (email, password) => {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json; charset=utf-8',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ email, password }),
   })
@@ -26,7 +26,7 @@ export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json; charset=utf-8',
+      'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
     },
   })
