8ad6657ec525a0573260041280855e707bc45220
Maksim Dav <maksim.dav@mail.ru>
2022-07-25 21:16:46 +0800

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { limiter, options } = require('./utils/constants');
+const { limiter } = require('./utils/constants');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const signup = require('./routes/signup');
@@ -20,6 +20,17 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://frontend.mesto.students.nomoredomains.xyz',
+    'http://frontend.mesto.students.nomoredomains.xyz',
+  ],
+  credentials: true,
+};
+
+app.use('*', cors(options));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -27,11 +38,9 @@ mongoose.connect(DB_CONN, {
   useNewUrlParser: true,
 });
 
-app.use('*', cors());
-
 app.use(requestLogger);
 
-//app.use(helmet());
+app.use(helmet());
 app.use(limiter);
 
 app.get('/crash-test', () => {
