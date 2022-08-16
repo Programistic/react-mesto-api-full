fdf7d85cdc0d834662c94d203079478dc0f3b71a
Maksim Dav <maksim.dav@mail.ru>
2022-08-16 20:55:27 +0800

    fix cors

--- backend/middlewares/preflight.js
+++ backend/middlewares/preflight.js
@@ -11,9 +11,6 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
   } else {
