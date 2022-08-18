675198f89c00c0663346d16fe56c2bb6db10c9fa
Maksim Dav <maksim.dav@mail.ru>
2022-08-18 22:34:54 +0800

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,28 +1,27 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-/*
+
 const allowedCors = [
   'https://frontend.mesto.students.nomoredomains.xyz',
   'http://frontend.mesto.students.nomoredomains.xyz',
   'https://localhost:3000',
   'http://localhost:3000',
 ];
-*/
 
 module.exports = (req, res, next) => {
   const { method } = req;
-  // const { origin } = req.headers;
+  const { origin } = req.headers;
   // const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
-    // if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-    // }
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', '*');
     res.end();
     return;
   }
-  // if (allowedCors.includes(origin)) {
-  res.header('Access-Control-Allow-Origin', '*');
-  // }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   next();
 };
