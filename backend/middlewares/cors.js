38357ad02d9f459e4409d9f936176a97c837c4c3
Maksim Dav <maksim.dav@mail.ru>
2022-08-18 22:27:10 +0800

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,26 +1,28 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+/*
 const allowedCors = [
   'https://frontend.mesto.students.nomoredomains.xyz',
   'http://frontend.mesto.students.nomoredomains.xyz',
   'https://localhost:3000',
   'http://localhost:3000',
 ];
+*/
 
 module.exports = (req, res, next) => {
   const { method } = req;
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  // const { origin } = req.headers;
+  // const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
+    // if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+    // }
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', '*');
     res.end();
     return;
   }
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  res.header('Access-Control-Allow-Origin', '*');
+  // }
   next();
 };
