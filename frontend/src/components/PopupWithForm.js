650c8e5bd96299ccccd4ac408b49243a59b7d533
Maksim Dav <maksim.dav@mail.ru>
2022-09-02 22:17:30 +0800

    delete noValidate

--- frontend/src/components/PopupWithForm.js
+++ frontend/src/components/PopupWithForm.js
@@ -13,7 +13,7 @@ function PopupWithForm({
     <div className={`popup popup_role_${name} ${popupOpened}`}>
       <div className="popup__container">
         <h2 className="popup__heading">{title}</h2>
-        <form className="form" name={name} onSubmit={onSubmit} noValidate>
+        <form className="form" name={name} onSubmit={onSubmit}>
           {children}
           <button className="form__submit form__submit_theme_black form__submit_location_popup button" type="submit" aria-label={buttonText}>{buttonText}</button>
         </form>
